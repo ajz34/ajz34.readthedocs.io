7fe6e4bcd04337d831f8e210a7c1bbd037e3dc9a
ajz34 <ajz34@outlook.com>
2021-06-11 12:13:35 +0800

    transform style to sphinx-book-theme

--- source/conf.py
+++ source/conf.py
@@ -28,9 +28,9 @@ author = 'ajz34'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        "nbsphinx",
+        "myst_nb",
         "sphinx.ext.mathjax",
-        "sphinxcontrib.bibtex"
+        "sphinxcontrib.bibtex",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -58,7 +58,13 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/ajz34/ajz34.readthedocs.io",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "toc_title": "文档目录",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -68,8 +74,12 @@ html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
 
+html_logo = "logo.png"
+html_favicon = "logo.ico"
+
 nbsphinx_allow_errors = True
 nbsphinx_timeout = 720
+jupyter_execute_notebooks = "off"
 # https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
 master_doc = "index"
 
